c3ea6a927dbffd59f6873fa79a3ba9e0158070b2
Ivan Kyryliuk <s32785@pjwstk.edu.pl>
2026-05-27 14:34:20 +0300

    Namespace fixes

--- WebApplication1/Models/ViewModels.cs
+++ WebApplication1/Models/ViewModels.cs
@@ -1,6 +1,27 @@
-﻿namespace WebApplication1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
 {
-    public class ViewModels
+    public class RegisterViewModel
     {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [MinLength(8, ErrorMessage = "Password must be at least 8 characters long.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
     }
 }
